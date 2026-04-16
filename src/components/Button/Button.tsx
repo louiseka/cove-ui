fd7f214b5fef689688cac783bb151ed89a95341f
Louise Aldridge <louisealdridge88@gmail.com>
2026-04-16 15:25:33 +0100

    Rendering buttons and setting up docs layout

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,14 @@
 import type { ButtonProps } from "./Button.type";
+import styles from "./Button.module.css";
 
-const Button = ({ children, type = "button", disabled }: ButtonProps) => {
+const Button = ({
+  children,
+  type = "button",
+  disabled = false,
+}: ButtonProps) => {
   return (
     <>
-      <button type={type} disabled={disabled}>
+      <button className={styles.button} disabled={disabled} type={type}>
         {children}
       </button>
     </>
