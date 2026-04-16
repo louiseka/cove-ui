d00ea972248dfcf4f3e3902968a04d479c6c1d90
Louise Aldridge <louisealdridge88@gmail.com>
2026-04-16 15:24:45 +0100

    Rendering docs on components page

---
+++ src/pages/Components/Components.tsx
@@ -0,0 +1,14 @@
+import ButtonDoc from "../../components/Button/ButtonDoc.mdx";
+import styles from "./Components.module.css";
+
+const Components = () => {
+  return (
+    <>
+      <section className={styles.components}>
+        <ButtonDoc></ButtonDoc>
+      </section>
+    </>
+  );
+};
+
+export default Components;
