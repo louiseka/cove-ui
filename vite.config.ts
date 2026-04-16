4f8beab31561e2c5fc3c23de7d7608c1e6e83738
Louise Aldridge <louisealdridge88@gmail.com>
2026-04-16 15:24:23 +0100

    Installing mdx

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import mdx from "@mdx-js/rollup";
+import remarkGfm from "remark-gfm";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    mdx({
+      remarkPlugins: [remarkGfm],
+    }),
+  ],
+});
