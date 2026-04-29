59d1c0f6c5606da5028d56af428be83c17ab1f00
Louise Aldridge <louisealdridge88@gmail.com>
2026-04-29 14:30:14 +0100

    Created responsive navigation

--- src/components/SiteNav/SiteNav.tsx
+++ src/components/SiteNav/SiteNav.tsx
@@ -1,18 +1,30 @@
 import styles from "./SiteNav.module.css";
+import { useState } from "react";
+import { FaAngleDown, FaAngleUp } from "react-icons/fa6";
 
 const SiteNav = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <nav className={styles.nav}>
-      <ul className={styles.navList}>
-        <li className={styles.navItem}>
-          <a className={styles.navLinkActive} href="#">
-            Button
-          </a>
-        </li>
-        <li className={styles.navItem}>Modal</li>
-        <li className={styles.navItem}>Modal</li>
-      </ul>
-    </nav>
+    <div className={styles.navContainer}>
+      <button
+        className={styles.menuButton}
+        onClick={() => setOpen((prev) => !prev)}
+      >
+        Menu {open ? <FaAngleUp /> : <FaAngleDown />}
+      </button>
+      <nav className={`${styles.nav} ${open ? styles.open : ""}`}>
+        <ul className={styles.navList}>
+          <li className={styles.navItem}>
+            <a className={styles.navLinkActive} href="#">
+              Button
+            </a>
+          </li>
+          <li className={styles.navItem}>Modal</li>
+          <li className={styles.navItem}>Modal</li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
