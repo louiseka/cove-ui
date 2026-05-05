d72b2143cd061c184fc86a8083f75c474dff6438
Louise Aldridge <louisealdridge88@gmail.com>
2026-05-05 16:03:03 +0100

    Updating props and input examples

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,18 +1,12 @@
 import type { InputProps } from "./Input.type";
 import styles from "./Input.module.css";
 
-const Input = ({
-  children,
-  id,
-  name,
-  type,
-  required = false,
-  size = 30,
-}: InputProps) => {
+const Input = ({ children, id, name, type, required = false }: InputProps) => {
   return (
     <div>
       <label className={styles.label} htmlFor={id}>
-        {children}
+        <span>{children}</span>
+        {required && <span className={styles.labelRequired}> (Required)</span>}
       </label>
       <input
         className={styles.input}
@@ -20,7 +14,6 @@ const Input = ({
         type={type}
         id={id}
         name={name}
-        size={size}
       />
     </div>
   );
