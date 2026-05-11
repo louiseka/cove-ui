2113fbfcb85c709320bbdbc22b4bee470c377e20
Louise Aldridge <louisealdridge88@gmail.com>
2026-05-11 15:42:35 +0100

    tests for passwordinput

--- src/components/PasswordInput/PasswordInput.test.tsx
+++ src/components/PasswordInput/PasswordInput.test.tsx
@@ -1,5 +1,6 @@
 import PasswordInput from "./PasswordInput";
 
+import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
 
 describe("PasswordInput", () => {
@@ -12,8 +13,53 @@ describe("PasswordInput", () => {
     expect(screen.getByText("Your Password")).toBeInTheDocument();
   });
 
-  test.todo("renders a password input by default");
-  test.todo("is required when required prop is true");
-  test.todo("shows the show password button when showToggle is true");
-  test.todo("changes to type text when show password is clicked");
+  it("renders a password input by default", () => {
+    render(
+      <PasswordInput id="password" name="password" required>
+        Your Password
+      </PasswordInput>,
+    );
+    const passwordInput = screen.getByLabelText("Your Password (Required)");
+    expect(passwordInput).toHaveAttribute("type", "password");
+  });
+
+  it("is required when required prop is true", () => {
+    render(
+      <PasswordInput id="password" name="password" required>
+        Your Password
+      </PasswordInput>,
+    );
+    const passwordInput = screen.getByLabelText("Your Password (Required)");
+    expect(passwordInput).toBeRequired();
+  });
+
+  it("shows the show password button when showToggle is true", () => {
+    render(
+      <PasswordInput id="show-password" name="show-password" showToggle>
+        Your Password
+      </PasswordInput>,
+    );
+    expect(
+      screen.getByRole("button", {
+        name: /show password/i,
+      }),
+    ).toBeInTheDocument();
+  });
+
+  it("changes to type text when show password is clicked", async () => {
+    const user = userEvent.setup();
+    render(
+      <PasswordInput id="show-password" name="show-password" showToggle>
+        Your Password
+      </PasswordInput>,
+    );
+    const passwordInput = screen.getByLabelText(/your password/i);
+    expect(passwordInput).toHaveAttribute("type", "password");
+
+    const button = screen.getByRole("button", {
+      name: /show password/i,
+    });
+    await user.click(button);
+    expect(passwordInput).toHaveAttribute("type", "text");
+  });
 });
