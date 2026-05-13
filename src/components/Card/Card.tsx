95273723f5a442a54123ca945e262af6f27ca54f
Louise Aldridge <louisealdridge88@gmail.com>
2026-05-13 14:19:43 +0100

    Defining card types and using them

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,24 +1,29 @@
-import oceanImage from "../../assets/images/ocean.webp";
+import type { CardProps } from "./Card.type";
 import styles from "./Card.module.css";
 
-const Card = () => {
+const Card = ({
+  img,
+  imgAlt,
+  title,
+  description,
+  buttonText,
+  link,
+}: CardProps) => {
   return (
     <>
       <article className={styles.card}>
-        <div className={styles.cardImgContainer}>
-          <img
-            className={styles.cardImg}
-            src={oceanImage}
-            alt="A close up of the ocean"
-          />
-        </div>
+        {img && (
+          <div className={styles.cardImgContainer}>
+            <img className={styles.cardImg} src={img} alt={imgAlt} />
+          </div>
+        )}
         <div className={styles.cardDetails}>
-          <h3>The Ocean</h3>
-          <p className={styles.cardText}>
-            A bunch and a load more bunch of words about the ocean.
-          </p>
-          <button className={styles.cardButton}>Book Now</button>
-          <a></a>
+          <h3>{title}</h3>
+          <p className={styles.cardText}>{description}</p>
+          {buttonText && (
+            <button className={styles.cardButton}>{buttonText}</button>
+          )}
+          {link && <a>{link}</a>}
         </div>
       </article>
     </>
---
+++ src/components/Card/Card.type.ts
@@ -0,0 +1,8 @@
+export interface CardProps {
+  img?: string;
+  imgAlt?: string;
+  title: string;
+  description: string;
+  buttonText?: string;
+  link?: string;
+}
