153e75ef94af2a25dfdbdb8a95184b0de238a82f
Louise Aldridge <louisealdridge88@gmail.com>
2026-05-13 11:06:47 +0100

    Creating and styling card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,9 +1,30 @@
+import oceanImage from "../../assets/images/ocean.webp";
+import styles from "./Card.module.css";
+
 const Card = () => {
   return (
     <>
-      <p>This is card</p>
+      <article className={styles.card}>
+        <div className={styles.cardImgContainer}>
+          <img
+            className={styles.cardImg}
+            src={oceanImage}
+            alt="A close up of the ocean"
+          />
+        </div>
+        <div className={styles.cardDetails}>
+          <h3>The Ocean</h3>
+          <p className={styles.cardText}>
+            A bunch and a load more bunch of words about the ocean.
+          </p>
+          <button className={styles.cardButton}>Book Now</button>
+          <a></a>
+        </div>
+      </article>
     </>
   );
 };
 
 export default Card;
+
+// maybe add some label/info points
