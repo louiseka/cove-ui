4b79b31d788db4a6c80a9df2b12af186d0d5092e
Louise Aldridge <louisealdridge88@gmail.com>
2026-05-20 15:06:18 +0100

    Adding test todos

---
+++ src/components/Card/Card.test.tsx
@@ -0,0 +1,11 @@
+import Card from "./Card";
+
+describe("Card", () => {
+  test.todo("It renders image and alt text");
+  test.todo("It renders title and text");
+  test.todo("It renders button");
+  test.todo("It renders link");
+  test.todo("It does not render image when img is not provided");
+  test.todo("It does not render button when buttonText is not provided");
+  test.todo("It does not render link when link is not provided");
+});
