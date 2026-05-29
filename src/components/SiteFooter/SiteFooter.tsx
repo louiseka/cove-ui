962409b877f435d5274144e00c0c061f29a6d0ea
Louise Aldridge <louisealdridge88@gmail.com>
2026-05-29 13:40:20 +0100

    Styling scroll to top button

--- src/components/SiteFooter/SiteFooter.tsx
+++ src/components/SiteFooter/SiteFooter.tsx
@@ -1,9 +1,27 @@
 import styles from "./SiteFooter.module.css";
 
+import { FaAnglesUp } from "react-icons/fa6";
+
 const SiteFooter = () => {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    console.log("button clicked");
+  };
+
   return (
     <footer className={styles.footer}>
-      <p>Built by Louise Aldridge </p>
+      <div>
+        <p>Built by Louise Aldridge </p>
+      </div>
+      <div>
+        <button
+          className={styles.footerButton}
+          onClick={scrollToTop}
+          aria-label="Back to the top of the page"
+        >
+          <FaAnglesUp />
+        </button>
+      </div>
     </footer>
   );
 };
