641a38a11251f89f35aa110e9564547c9b189eda
Louise Aldridge <louisealdridge88@gmail.com>
2026-06-05 13:17:56 +0100

    Using types in component

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,50 +1,23 @@
+import type { AccordionProps } from "./Accordion.type";
 import styles from "./Accordion.module.css";
 
 import { FaAngleDown } from "react-icons/fa6";
 
-const Accordion = () => {
+const Accordion = ({ heading, items }: AccordionProps) => {
   return (
     <section className={styles.accordionWrapper}>
-      <h2>Frequently Asked Questions</h2>
-      <details className={styles.accordionDetails}>
-        <summary className={styles.accordionSummary}>
-          <h3> How much is the tour?</h3>
-          <span className={styles.accordionIcon}>
-            <FaAngleDown />
-          </span>
-        </summary>
-        <p className={styles.accordionText}>
-          ras sem ligula, bibendum in scelerisque ac, vestibulum pulvinar nulla.
-          Duis eleifend sed dolor ut tempor. Suspendisse potenti. Integer
-          elementum tristi
-        </p>
-      </details>
-      <details className={styles.accordionDetails}>
-        <summary className={styles.accordionSummary}>
-          <h3> How much is the tour?</h3>
-          <span className={styles.accordionIcon}>
-            <FaAngleDown />
-          </span>
-        </summary>
-        <p>
-          ras sem ligula, bibendum in scelerisque ac, vestibulum pulvinar nulla.
-          Duis eleifend sed dolor ut tempor. Suspendisse potenti. Integer
-          elementum tristi
-        </p>
-      </details>
-      <details className={styles.accordionDetails}>
-        <summary className={styles.accordionSummary}>
-          <h3> How much is the tour?</h3>
-          <span className={styles.accordionIcon}>
-            <FaAngleDown />
-          </span>
-        </summary>
-        <p>
-          ras sem ligula, bibendum in scelerisque ac, vestibulum pulvinar nulla.
-          Duis eleifend sed dolor ut tempor. Suspendisse potenti. Integer
-          elementum tristi
-        </p>
-      </details>
+      {heading && <h2>{heading}</h2>}
+      {items.map((item, index) => (
+        <details key={index} className={styles.accordionDetails}>
+          <summary className={styles.accordionSummary}>
+            <h3> {item.title}</h3>
+            <span className={styles.accordionIcon}>
+              <FaAngleDown />
+            </span>
+          </summary>
+          <p className={styles.accordionText}>{item.content}</p>
+        </details>
+      ))}
     </section>
   );
 };
