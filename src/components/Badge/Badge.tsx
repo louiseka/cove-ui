9d20942dd265e62d7e168dc69a307e33364fd605
Louise Aldridge <louisealdridge88@gmail.com>
2026-07-09 15:57:57 +0100

    Adding types to badge

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,7 +1,13 @@
+import type { BadgeProps } from "./Badge.type";
 import styles from "./Badge.module.css";
 
-const Badge = () => {
-  return <span className={styles.badge}> Completed</span>;
+const Badge = ({ children, icon, variant, live }: BadgeProps) => {
+  return (
+    <span aria-live={live} className={`${styles.badge} ${styles[variant]}`}>
+      {icon && <span aria-hidden="true">{icon}</span>}
+      {children}
+    </span>
+  );
 };
 
 export default Badge;
