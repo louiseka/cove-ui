77991104a2b5ad7e445310e944583371fd1e4f26
Louise Aldridge <louisealdridge88@gmail.com>
2026-07-13 15:06:10 +0100

    Semantic structure for select input

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,5 +1,18 @@
 const Select = () => {
-  return <p>This is select input</p>;
+  return (
+    <>
+      <label htmlFor="pet-select">Choose a trip:</label>
+      <select name="pets" id="pet-select">
+        <option value="dog">Dog</option>
+        <option value="cat">Cat</option>
+        <option value="hamster">Hamster</option>
+        <option value="parrot">Parrot</option>
+        <option value="spider">Spider</option>
+        <option value="goldfish">Goldfish</option>
+      </select>
+      ;
+    </>
+  );
 };
 
 export default Select;
