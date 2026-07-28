c93e16ebccf52e66767ecc7c2a9400d5352fcb50
Louise Aldridge <louisealdridge88@gmail.com>
2026-07-28 13:39:35 +0100

    Writing test todos

---
+++ src/components/Select/Select.test.tsx
@@ -0,0 +1,9 @@
+import Select from "./Select";
+
+describe("Select", () => {
+  test.todo("it renders a label correctly");
+  test.todo("it renders the options");
+  test.todo("associates label with input via htmlFor");
+  test.todo("it is disabled when disabled is true");
+  test.todo("throws error message when error is true");
+});
